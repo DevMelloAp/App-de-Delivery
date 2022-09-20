451fed832dd10077401890f52a14434f4d15378d
imarmendes <imarmendes@gmail.com>
2022-09-20 19:44:16 -0300

    Adicinar midleware de erro ao backend

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,12 @@
+require('express-async-errors');
 const express = require('express');
+const loginRouter = require('../routes/userRoutes');
+
+const errorMiddleware = require('../middlewares/error');
 
 const app = express();
-
 app.get('/coffee', (_req, res) => res.status(418).end());
+app.use('/login', loginRouter);
 
+app.use(errorMiddleware);
 module.exports = app;
