abf69f423d488e83bd15463dc9020c7ca1dba7af
Emerson <emerapmelo@gmail.com>
2022-09-21 18:58:26 -0300

    Corrigidos os erros de lint

--- back-end/src/middlewares/registerValidate.js
+++ back-end/src/middlewares/registerValidate.js
@@ -1,25 +1,12 @@
-const registerValidate = (email, password, name) => {
-if (!email || !password) {
-const e = new Error('All fields must be filled');
-e.name = 'ValidationError';
-throw e;
-}
-const regex = /\S+@\S+\.\S+/;
-if (!regex.test(email)) {
-const e = new Error('Incorrect email or password');
-e.name = 'NotFoundError';
-throw e;
-}
-if (password.length < 6) {
-const e = new Error('Password must be at least 6 characters long');
-e.name = 'NotFoundError';
-throw e;
-}
+const userValidate = require('./userValidate');
 
-if (name.length < 12) {
-const e = new Error('Name must be at least 12 characters long');
-e.name = 'NotFoundError';
-throw e;
+const registerValidate = (email, password, name) => {
+    userValidate(email, password);
+
+    if (name.length < 12) {
+        const e = new Error('Name must be at least 12 characters long');
+        e.name = 'NotFoundError';
+        throw e;
 }
 };
 
--- back-end/src/services/userServises.js
+++ back-end/src/services/userServises.js
@@ -7,18 +7,26 @@ const db = require('../database/models');
 const { JwtServiceSign } = require('./JwtService');
 
 const create = async ({ name, email, password }) => {
-registerValidate(email, password, name);
-const role = 'customer';
-const users = await User.findAll();
-const emailList = users.map((it) => it.email);
-const nameList = users.map((it) => it.name);
-if ((emailList).includes(email) || nameList.includes(name)) {
-const e = new Error('User already registered');
-e.name = 'ConflictError';
-throw e;
-}
-const user = await User.create({ name, email, password, role });
-return user;
+  registerValidate(email, password, name);
+  const role = 'customer';
+  const users = await User.findAll();
+  const emailList = users.map((it) => it.email);
+  const nameList = users.map((it) => it.name);
+  if ((emailList).includes(email) || nameList.includes(name)) {
+  const e = new Error('User already registered');
+  e.name = 'ConflictError';
+  throw e;
+  }
+  const user = await User.create({ name, email, password, role });
+
+  const token = JwtServiceSign({ id: user.id, email: user.email });
+
+  return {
+    name: user.name,
+    email: user.email,
+    role: user.role,
+    token, 
+  };
 };
 
 const loginService = async (email, password) => {
