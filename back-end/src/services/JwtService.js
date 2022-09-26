809dd4e2080e6584e4cfcaee51429706fba7f350
Rafael Moraes Dias <rafasjbvb@yahoo.com.br>
2022-09-26 17:46:07 -0300

    aaa

--- back-end/src/services/JwtService.js
+++ back-end/src/services/JwtService.js
@@ -1,16 +1,16 @@
 const jwt = require('jsonwebtoken');
-// const fs = require('fs');
+const fs = require('fs');
 
-// const senha =  fs.readFileSync("back-end/jwt.evaluation.key", "utf-8" );
-// console.log(senha);
+const senha =  fs.readFileSync("jwt.evaluation.key", "utf-8" );
+console.log(senha);
 
 const JwtServiceSign =  (id, email) => {
 
-  const token = jwt.sign({ id, email }, process.env.JWT_SECRET || "secret_key");
+  const token = jwt.sign({ id, email }, process.env.JWT_SECRET || senha);
   return token;
 };
   const JwtServiceSignDecode = (token) => {
-    const tokenVerify = jwt.verify(token, process.env.JWT_SECRET || "secret_key");
+    const tokenVerify = jwt.verify(token, process.env.JWT_SECRET || senha);
     return tokenVerify;
   };
 
