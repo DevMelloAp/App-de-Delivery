e3c8d34c3a19d1682e8abbb96d6cb74fcfa4bd0f
imarmendes <imarmendes@gmail.com>
2022-09-27 17:03:50 -0300

    Requisitos 15 e 16 passando

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 
+import Checkout from './components/checkout';
 import Login from './Pages/Login';
 import Products from './Pages/Products';
 import Register from './Pages/Register';
@@ -13,7 +14,7 @@ function App() {
       <Route exact path="/customer/products" element={ <Products /> } />
       <Route exact path="/login" element={ <Login /> } />
       <Route exact path="/register" element={ <Register /> } />
-      <Route exact path="/checkout" element={ <Register /> } />
+      <Route exact path="/customer/checkout" element={ <Checkout /> } />
 
     </Routes>
   );
--- front-end/src/components/checkout.jsx
+++ front-end/src/components/checkout.jsx
@@ -1,22 +1,33 @@
 import Button from '@material-ui/core/Button';
-import { cartTotal } from '../utils/cartLocalsotorage';
+import { useEffect } from 'react';
+// import { cartTotal } from '../utils/cartLocalsotorage';
+import { useSelector } from 'react-redux';
 
 export default function Checkout() {
-  const cart = cartTotal();
+  const cartTotal = useSelector((state) => state.default.total);
+  console.log(cartTotal, 'cartTotal');
+  useEffect(() => {
+
+  }, [cartTotal]);
+  // const cart = cartTotal();
   return (
     <div>
       <Button
-        href="/checkout"
+        href="/customer/checkout"
         variant="contained"
         color="primary"
         data-testid="customer_products__checkout-bottom-value"
       >
-        Ver Carrinho :
-        {' '}
-        {cart}
-        {' '}
-        {}
+
+        <button
+          type="button"
+          disabled={ cartTotal === 0 }
+          data-testid="customer_products__button-cart"
+        >
+          {(+cartTotal).toFixed(2).replace('.', ',')}
+        </button>
       </Button>
+
     </div>
   );
 }
