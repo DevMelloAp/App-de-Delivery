55212d1b64fbbe6ba047dee863891fde22ea861f
Carlos Afonso <carlos.flach15@gmail.com>
2022-10-03 17:48:37 -0300

    Fix: route saller header

--- front-end/src/components/SalesOrdersHeader.jsx
+++ front-end/src/components/SalesOrdersHeader.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import { useNavigate } from 'react-router-dom';
 import styles from '../styles/header.module.css';
 import { getToLocalstorage, removeToLocalstorage } from '../utils/userLocalstorage';
 
 export default function SalesOrdersHeader() {
   const { name } = getToLocalstorage();
+  const navigate = useNavigate();
+
   return (
     <header>
       <nav className={ styles.header }>
@@ -12,8 +15,8 @@ export default function SalesOrdersHeader() {
           <Button
             variant="contained"
             color="primary"
-            href="/orders"
             data-testid="customer_products__element-navbar-link-orders"
+            onClick={ () => navigate('/customer/products') }
           >
             PEDIDOS
           </Button>
